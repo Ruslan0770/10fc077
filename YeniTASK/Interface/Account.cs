b1d6bdd51e8abed2949740c6cf9c93fa63119c7f
Ruslan0770 <ruslania@code.edu.az>
2022-04-19 13:49:02 -0700

    Add project files.

---
+++ YeniTASK/Interface/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YeniTASK.Interface
+{
+    interface Account
+    {
+       
+        
+            public bool PasswordChecker(string password);
+            public void ShowInfo();
+
+
+
+
+        
+    }
+}
---
+++ YeniTASK/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using YeniTASK.Models;
+
+namespace YeniTASK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            Console.WriteLine("ad daxil et");
+            string a = Console.ReadLine();
+            Console.WriteLine("email da");
+            string b = Console.ReadLine();
+            user.ShowInfo(a,b);
+            Console.WriteLine("Sifreni daxil et");
+            user.PasswordChecker(Console.ReadLine());
+        }
+    }
+}
